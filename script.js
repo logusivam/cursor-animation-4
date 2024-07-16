0640f3cf6c56d29c3bc3a26c92e0bf7ffdce4abf
Loganathan G P <logusivam26@gmail.com>
2024-07-16 11:28:01 +0000

    Jul 16, 2024, 4:58 PM

--- script.js
+++ script.js
@@ -4,5 +4,7 @@
     document.addEventListener('mousemove', e => {
         cursor.setAttribute('style', `top: ${e.pageY - 25}px; left: ${e.pageX - 25}px;`);
     });
-    document.addEventListener('click', ())
+    document.addEventListener('click', () => {
+        console.log("%c Click...!!!", "font-size: 20px")
+    })
 })
