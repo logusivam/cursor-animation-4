f05ae7e89fa63b8b4ad0b6d24bab3aed949f92fa
Loganathan G P <logusivam26@gmail.com>
2024-07-16 11:27:06 +0000

    Jul 16, 2024, 4:57 PM

---
+++ script.js
@@ -0,0 +1,8 @@
+(() => {
+    const cursor = document.querySelector('.cursor');
+
+    document.addEventListener('mousemove', e => {
+        cursor.setAttribute('style', `top: ${e.pageY - 25}px; left: ${e.pageX - 25}px;`);
+    });
+    document.addEventListener('click', ())
+})
