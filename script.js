1b65786c3f1dd21cb712af246792eb3f2b571a13
Loganathan G P <logusivam26@gmail.com>
2024-07-16 11:32:51 +0000

    Jul 16, 2024, 5:02 PM

--- script.js
+++ script.js
@@ -7,6 +7,11 @@
     document.addEventListener('click', () => {
         console.log("%c Click...!!!", "font-size: 20px; color: mediumspringgreen;");
 
-        cursor.classList.add
-    })
-})
+        cursor.classList.add('cursor--expand');
+        console.log(cursor.classList);
+
+        setTimeout(() => {
+            cursor.classList.remove('cursor--expand');
+        }, 500);
+    });
+})();
